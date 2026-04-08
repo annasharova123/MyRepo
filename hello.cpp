2e19b8412d377115ca00feb15bc1bb5dfc3cc5eb
Anna <annasharova2321@gmail.com>
2026-04-08 23:01:51 +0300

    Вычисляем число фибоначчи начиная с (0,1)

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 long long fib(int n) {
-    if (n <= 2) return 1;
+    if (n == 1) return 0;
+    if (n == 2) return 1;
     return fib(n - 1) + fib(n - 2);
 }
 
@@ -10,5 +11,6 @@ int main() {
     int n;
     cout << "Enter n: ";
     cin >> n;
+
     cout << fib(n) << endl;
 }
